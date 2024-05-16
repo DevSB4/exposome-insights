b18c9a0e3b965f2e39db2989f0e843e0c8b6d9c6
DevSB4 <devproject1245@gmail.com>
2024-05-16 09:50:29 -0400

    Updated vite.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,25 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server: {
+//     host: true,
+//     port: 3000,
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:4000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -15,7 +33,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://exposome-insights-server.vercel.app",
         changeOrigin: true,
       },
     },
