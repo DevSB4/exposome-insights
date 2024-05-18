7ff87c03eda5498c3e7a2d18849c2481f402fa5d
DevSB4 <devproject1245@gmail.com>
2024-05-18 12:22:20 -0400

    Imported useNavigate in useRegister.jsx

--- client/src/hooks/useRegister.jsx
+++ client/src/hooks/useRegister.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const useRegister = () => {
   const [error, setError] = useState(null);
